43d5bd638c0e36272ba5b237ecd542f5110d5606
olatunjiemanuel <olatunjiemanuel15@gmail.com>
2025-11-26 14:00:06 +0000

    Set Up IU for custom searchbar, destination cards and button

--- travel-companion-frontend/src/App.tsx
+++ travel-companion-frontend/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
+import "./assets/colors.css";
 import './App.css'
+import SearchBar from './components/SeachBar'
+import CustomButton from './components/CustomButton'
+import DestinationCard from './components/DestinationCards/DestinationCard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [searchValue, setSearchValue] = useState("");
+    return (
+        <>
+            <SearchBar placeHolder="Search for a place to visit ..." value={searchValue} onChangeText={(e) => {
+                setSearchValue(e.target.value)
+            }}/>
+            <CustomButton buttonName="Example"/>
+            <DestinationCard city="Paris" country="France"/>
+        </>
+    )
 }
 
 export default App
